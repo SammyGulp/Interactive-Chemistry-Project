b79299005a9f58f6b7b612830f563aea5af497f3
SammyGulp <133907356+SammyGulp@users.noreply.github.com>
2023-10-22 22:34:50 -0700

    Added all the pages I'll need and did a crap ton of work.

---
+++ pageNav.js
@@ -0,0 +1,19 @@
+function goTo(page) {
+    // The pages that you can be sent to
+    var resource = "";
+    
+    switch (page) {
+case "table":
+resource = "https://sammygulp.github.io/Interactive-Chemistry-Project/pTable";
+break;
+case "journal":
+resource = "https://sammygulp.github.io/Interactive-Chemistry-Project/journal";
+break;
+case "experiment":
+resource = "https://sammygulp.github.io/Interactive-Chemistry-Project/exps";
+break;
+    
+    
+    //Should replace the site with another.
+    window.open(resource, "_parent");
+}
